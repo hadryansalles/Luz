27f830d93c1471afd061cb166702e1fc4846bb22
Hadryan Salles <hadryansalles00@gmail.com>
2021-07-20 23:57:30 -0300

    created scene class

---
+++ source/Graphics/Scene.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <filesystem>
+
+class Scene {
+private:
+    static inline std::filesystem::path path = "assets/Default.Luzscene";
+    static inline bool dirty = true;
+    static inline bool autoReloadFiles = true;
+
+public:
+    static void Init();
+    static void Save();
+    static void Destroy();
+    static void OnImgui();
+};
