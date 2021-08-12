4bf7a2947ffb9251d3e2ed7b5c27b28e9cbe300f
Hadryan Salles <hadryansalles00@gmail.com>
2021-08-12 17:28:44 -0300

    refactoring unlit graphics pipeline creation

---
+++ source/Graphics/UnlitGraphicsPipeline.cpp
@@ -0,0 +1,100 @@
+#include "Luzpch.hpp"
+
+#include "Mesh.hpp"
+#include "UnlitGraphicsPipeline.hpp"
+#include "FileManager.hpp"
+#include "SwapChain.hpp"
+
+void UnlitGraphicsPipeline::Setup() {
+    desc.name = "Unlit";
+
+    desc.shaderStages.resize(2);
+    desc.shaderStages[0].shaderBytes = FileManager::ReadRawBytes("bin/shader.vert.spv");
+    desc.shaderStages[0].stageBit = VK_SHADER_STAGE_VERTEX_BIT;
+    desc.shaderStages[1].shaderBytes = FileManager::ReadRawBytes("bin/shader.frag.spv");
+    desc.shaderStages[1].stageBit = VK_SHADER_STAGE_FRAGMENT_BIT;
+
+    desc.bindingDesc = MeshVertex::getBindingDescription();
+    desc.attributesDesc = MeshVertex::getAttributeDescriptions();
+
+    desc.rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    // fragments beyond near and far planes are clamped to them
+    desc.rasterizer.depthClampEnable = VK_FALSE;
+    desc.rasterizer.rasterizerDiscardEnable = VK_FALSE;
+    desc.rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
+    // line thickness in terms of number of fragments
+    desc.rasterizer.lineWidth = 1.0f;
+    desc.rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+    desc.rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
+    desc.rasterizer.depthBiasEnable = VK_FALSE;
+    desc.rasterizer.depthBiasConstantFactor = 0.0f;
+    desc.rasterizer.depthBiasClamp = 0.0f;
+    desc.rasterizer.depthBiasSlopeFactor = 0.0f;
+
+    desc.multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+    desc.multisampling.rasterizationSamples = SwapChain::GetNumSamples();
+    desc.multisampling.sampleShadingEnable = VK_FALSE;
+    desc.multisampling.minSampleShading = 0.5f;
+    desc.multisampling.pSampleMask = nullptr;
+
+    desc.depthStencil.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+    desc.depthStencil.depthTestEnable = VK_TRUE;
+    desc.depthStencil.depthWriteEnable = VK_TRUE;
+    desc.depthStencil.depthCompareOp = VK_COMPARE_OP_LESS;
+    desc.depthStencil.depthBoundsTestEnable = VK_FALSE;
+    desc.depthStencil.minDepthBounds = 0.0f;
+    desc.depthStencil.maxDepthBounds = 1.0f;
+    desc.depthStencil.stencilTestEnable = VK_FALSE;
+    desc.depthStencil.front = {};
+    desc.depthStencil.back = {};
+
+    desc.colorBlendAttachment.colorWriteMask =  VK_COLOR_COMPONENT_R_BIT;
+    desc.colorBlendAttachment.colorWriteMask |= VK_COLOR_COMPONENT_G_BIT;
+    desc.colorBlendAttachment.colorWriteMask |= VK_COLOR_COMPONENT_B_BIT;
+    desc.colorBlendAttachment.colorWriteMask |= VK_COLOR_COMPONENT_A_BIT;
+    desc.colorBlendAttachment.blendEnable = VK_TRUE;
+    desc.colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
+    desc.colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+    desc.colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;
+    desc.colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+    desc.colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
+    desc.colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;
+
+    desc.colorBlendState.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+    desc.colorBlendState.logicOpEnable = VK_FALSE;
+    desc.colorBlendState.logicOp = VK_LOGIC_OP_COPY;
+    desc.colorBlendState.attachmentCount = 1;
+    desc.colorBlendState.pAttachments = &desc.colorBlendAttachment;
+    desc.colorBlendState.blendConstants[0] = 0.0f;
+    desc.colorBlendState.blendConstants[1] = 0.0f;
+    desc.colorBlendState.blendConstants[2] = 0.0f;
+    desc.colorBlendState.blendConstants[3] = 0.0f;
+
+    desc.bindings.resize(2);
+    desc.bindings[0].binding = 0;
+    desc.bindings[0].descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    desc.bindings[0].descriptorCount = 1;
+    // here we specify in which shader stages the buffer will by referenced
+    desc.bindings[0].stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+    // only relevant for image sampling related descriptors
+    desc.bindings[0].pImmutableSamplers = nullptr;
+
+    desc.bindings[1].binding = 1;
+    desc.bindings[1].descriptorCount = 1;
+    desc.bindings[1].descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+    desc.bindings[1].pImmutableSamplers = nullptr;
+    desc.bindings[1].stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
+}
+
+void UnlitGraphicsPipeline::Create() {
+    desc.multisampling.rasterizationSamples = SwapChain::GetNumSamples();
+    GraphicsPipelineManager::CreatePipeline(desc, res);
+}
+
+void UnlitGraphicsPipeline::Destroy() {
+    GraphicsPipelineManager::DestroyPipeline(res);
+}
+
+void UnlitGraphicsPipeline::OnImgui() {
+    GraphicsPipelineManager::OnImgui(desc, res);
+}
--- source/Vulkan/BuffersManager.cpp
+++ source/Vulkan/BuffersManager.cpp
@@ -1,11 +1,11 @@
 #include "Luzpch.hpp"
 
-#include "Buffers.hpp"
+#include "BuffersManager.hpp"
 #include "LogicalDevice.hpp"
 #include "PhysicalDevice.hpp"
 #include "Instance.hpp"
 
-void Buffers::Create(const BufferDesc& desc, BufferResource& res) {
+void BuffersManager::Create(const BufferDesc& desc, BufferResource& res) {
     auto device = LogicalDevice::GetVkDevice();
     auto allocator = Instance::GetAllocator();
 
@@ -33,12 +33,12 @@ void Buffers::Create(const BufferDesc& desc, BufferResource& res) {
     vkBindBufferMemory(device, res.buffer, res.memory, 0);
 }
 
-void Buffers::Destroy(BufferResource& res) {
+void BuffersManager::Destroy(BufferResource& res) {
     vkDestroyBuffer(LogicalDevice::GetVkDevice(), res.buffer, Instance::GetAllocator());
     vkFreeMemory(LogicalDevice::GetVkDevice(), res.memory, Instance::GetAllocator());
 }
 
-void Buffers::Copy(VkBuffer src, VkBuffer dst, VkDeviceSize size) {
+void BuffersManager::Copy(VkBuffer src, VkBuffer dst, VkDeviceSize size) {
     auto commandBuffer = LogicalDevice::BeginSingleTimeCommands();
 
     VkBufferCopy copyRegion{};
@@ -50,43 +50,43 @@ void Buffers::Copy(VkBuffer src, VkBuffer dst, VkDeviceSize size) {
     LogicalDevice::EndSingleTimeCommands(commandBuffer);
 }
 
-void Buffers::Update(BufferResource& res, void* data, VkDeviceSize size) {
+void BuffersManager::Update(BufferResource& res, void* data, VkDeviceSize size) {
     void* dst;
     vkMapMemory(LogicalDevice::GetVkDevice(), res.memory, 0, size, 0, &dst);
     memcpy(dst, data, size);
     vkUnmapMemory(LogicalDevice::GetVkDevice(), res.memory);
 }
 
-void Buffers::CreateStaged(const BufferDesc& desc, BufferResource& res, void* data) {
+void BuffersManager::CreateStaged(const BufferDesc& desc, BufferResource& res, void* data) {
     BufferResource staging;
-    Buffers::Create(desc, res);
+    BuffersManager::Create(desc, res);
     CreateStagingBuffer(staging, data, desc.size);
-    Buffers::Copy(staging.buffer, res.buffer, desc.size);
-    Buffers::Destroy(staging);
+    BuffersManager::Copy(staging.buffer, res.buffer, desc.size);
+    BuffersManager::Destroy(staging);
 }
 
-void Buffers::CreateStagingBuffer(BufferResource& res, void* data, VkDeviceSize size) {
+void BuffersManager::CreateStagingBuffer(BufferResource& res, void* data, VkDeviceSize size) {
     BufferDesc stagingDesc;
     stagingDesc.usage = VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
     stagingDesc.properties = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
     stagingDesc.size = size;
-    Buffers::Create(stagingDesc, res);
-    Buffers::Update(res, data, size);
+    BuffersManager::Create(stagingDesc, res);
+    BuffersManager::Update(res, data, size);
 }
 
-void Buffers::CreateIndexBuffer(BufferResource& res, void* data, VkDeviceSize size) {
+void BuffersManager::CreateIndexBuffer(BufferResource& res, void* data, VkDeviceSize size) {
     BufferDesc desc;
     desc.usage = VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT;
     desc.properties = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
     desc.size = size;
-    Buffers::CreateStaged(desc, res, data);
+    BuffersManager::CreateStaged(desc, res, data);
 }
 
-void Buffers::CreateVertexBuffer(BufferResource& res, void* data, VkDeviceSize size) {
+void BuffersManager::CreateVertexBuffer(BufferResource& res, void* data, VkDeviceSize size) {
     BufferDesc desc;
     desc.usage = VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
     desc.properties = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
     desc.size = size;
-    Buffers::CreateStaged(desc, res, data);
+    BuffersManager::CreateStaged(desc, res, data);
 }
 
